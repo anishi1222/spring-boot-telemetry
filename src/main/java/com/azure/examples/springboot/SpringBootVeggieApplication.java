63f315ba54893a071211cb4c4fb77656ee7235ea
Akihiro Nishikawa <a_nishi-anarch@bc4.so-net.ne.jp>
2025-06-11 00:28:21 +0900

    Update Dockerfiles and configuration to include application insights and simplify build process

--- src/main/java/com/azure/examples/springboot/SpringBootVeggieApplication.java
+++ src/main/java/com/azure/examples/springboot/SpringBootVeggieApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import com.azure.examples.springboot.service.VeggieService;
 
 @SpringBootApplication
 public class SpringBootVeggieApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootVeggieApplication.class, args);
+        SpringApplication.run(SpringBootVeggieApplication.class, args);
   }
 
   @Bean
